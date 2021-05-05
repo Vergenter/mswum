aa6079c30df70651866ff8584015f5d76d76aaee
Vergenter <okolok1998@gmail.com>
2021-05-05 02:01:21 +0200

    removed png and add comments to library

--- KnackpackData.py
+++ KnackpackData.py
@@ -12,7 +12,7 @@ weights =  np.random.rand(n_items)
 values = np.random.rand(n_items)
 items_size_threshold = 0.5
 
-@njit([boolean[::1](int32)])
+@njit([boolean[::1](int32)]) # return boolean array
 def encodeNumber(number):
     return np.array([bool(number & (1<<(n_items-1-x))) for x in prange(n_items)],dtype=boolean)
 
@@ -26,10 +26,10 @@ def scoreFunction(mask):
             score+=values[i]
     return -1 if final_weight > max_weight else score
 
-@njit([float32[::1]()])
+@njit([float32[::1]()]) # return float array
 def getScores():
     return np.array([scoreFunction(encodeNumber(x)) for x in prange(max_value)],dtype=float32)
 
-@njit([boolean[:,::1]()])
+@njit([boolean[:,::1]()]) # return 2d boolean array
 def getEncoded():
     return np.array([[boolean(number & (1<<(n_items-1-x))) for x in prange(n_items)] for number in prange(max_value)],dtype=boolean)
